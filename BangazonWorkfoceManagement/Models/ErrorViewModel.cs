ec89589f203993851421a6911bf30da2f4b6c2ab
Will Wilkinson <wfw40869@gmail.com>
2019-11-14 16:06:02 -0600

    Finished setting up boilerplate

---
+++ BangazonWorkfoceManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BangazonWorkfoceManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
