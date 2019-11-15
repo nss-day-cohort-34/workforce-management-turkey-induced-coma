2367027eb4d1fd2a6d73e1a31791ec415c847b1e
Will Wilkinson <wfw40869@gmail.com>
2019-11-15 13:46:43 -0600

    View employee list now works. Added a link to the navbar to allow the user to view a list of employees

---
+++ BangazonWorkfoceManagement/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkfoceManagement.Models
+{
+    public class Department
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Department Name")]
+        public string Name { get; set; }
+        public int Budget { get; set; }
+
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
---
+++ BangazonWorkfoceManagement/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkfoceManagement.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Surname")]
+        public string LastName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+        [Required]
+        [Display(Name = "Supervisor")]
+        public Boolean IsSupervisor { get; set; }
+    }
+}
