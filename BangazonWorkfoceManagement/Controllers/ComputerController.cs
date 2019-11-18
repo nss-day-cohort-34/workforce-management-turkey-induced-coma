969a489d8e426f0a8bc56c99b8469d11a345df01
Maggie Johnson <mmjohn09@icloud.com>
2019-11-18 10:26:16 -0600

    can view list of all computers

---
+++ BangazonWorkfoceManagement/Controllers/ComputerController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonWorkfoceManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonWorkfoceManagement.Controllers
+{
+    public class ComputerController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public ComputerController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: Computer
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT c.Id, c.PurchaseDate, c.DecomissionDate, c.Make, c.Manufacturer
+                                        FROM Computer c";
+
+                    var reader = cmd.ExecuteReader();
+                    List<Computer> computers = new List<Computer>();
+                  
+
+                    while (reader.Read())
+                    {
+
+
+
+
+                        if (reader.IsDBNull(reader.GetOrdinal("DecomissionDate")))
+                        {
+                            computers.Add(
+                                new Computer()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                    PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate")),
+                                    Make = reader.GetString(reader.GetOrdinal("Make")),
+                                    Manufacturer = reader.GetString(reader.GetOrdinal("Manufacturer")),
+
+                                });
+                            
+                        }
+                        else
+                        {
+                            computers.Add(
+                                new Computer()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                    PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate")),
+                                    DecomissionDate = reader.GetDateTime(reader.GetOrdinal("DecomissionDate")),
+                                    Make = reader.GetString(reader.GetOrdinal("Make")),
+                                    Manufacturer = reader.GetString(reader.GetOrdinal("Manufacturer")),
+
+                                });
+
+                        }
+
+                   
+                    }
+                    reader.Close();
+                    return View(computers);
+                }
+            }
+        }
+
+        // GET: Computer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Computer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Computer/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Computer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Computer/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Computer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Computer/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BangazonWorkfoceManagement/Models/Computer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkfoceManagement.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display (Name = "Purchase Date")]
+        public DateTime PurchaseDate { get; set; }
+        [Display (Name = "Decomission Date")]
+        public DateTime DecomissionDate { get; set; }
+        [Required]
+        public string Make { get; set; }
+        [Required]
+        public string Manufacturer { get; set; }
+    }
+}
