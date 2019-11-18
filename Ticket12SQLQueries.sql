fa08a8bd7f06eac784937fb509d17231f44494ea
curtis <crutchc@yahoo.com>
2019-11-18 13:56:15 -0600

    can delete

---
+++ Ticket12SQLQueries.sql
@@ -0,0 +1,3 @@
+﻿SELECT tp.Id, tp.Name, tp.StartDate, tp.EndDate, tp.MaxAttendees
+                                        FROM TrainingProgram tp
+                                        WHERE tp.id = 1;
