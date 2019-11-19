290c73658b3c05c6e930df079b4df6cab7a094a1
Will Wilkinson <wfw40869@gmail.com>
2019-11-19 13:50:19 -0600

    Completed Bryan's challenge to display budgets in a USD currency format

--- BangazonWorkfoceManagement/Models/Department.cs
+++ BangazonWorkfoceManagement/Models/Department.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace BangazonWorkfoceManagement.Models
 {
@@ -14,7 +15,14 @@ namespace BangazonWorkfoceManagement.Models
         [Display(Name = "Department Name")]
         public string Name { get; set; }
         public int Budget { get; set; }
-
+        public string BudgetAsCurrency
+        {
+            get
+            {
+                decimal result = Budget / 100m;
+                return result.ToString("C", CultureInfo.CurrentCulture);
+            }
+        }
         public List<Employee> Employees { get; set; } = new List<Employee>();
     }
 }
--- BangazonWorkfoceManagement/Models/ViewModels/DepartmentListViewModel.cs
+++ BangazonWorkfoceManagement/Models/ViewModels/DepartmentListViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace BangazonWorkfoceManagement.Models.ViewModels
 {
@@ -14,6 +15,14 @@ namespace BangazonWorkfoceManagement.Models.ViewModels
         [Display(Name = "Department Name")]
         public string Name { get; set; }
         public int Budget { get; set; }
+        public string BudgetAsCurrency
+        {
+            get
+            {
+                decimal result = Budget / 100m;
+                return result.ToString("C", CultureInfo.CurrentCulture);
+            }
+        }
         [Display(Name = "Employee Count")]
         public int?  EmployeeCount { get; set; }
     }
